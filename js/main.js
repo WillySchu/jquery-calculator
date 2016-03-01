f7ae9c47697b24012fe34895e702dcbd10c2de95
Will Schumacher <meintodengel@gmail.com>
2016-03-01 08:35:42 -0800

    Lint

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $("span").click(function(){
+  $("span").click(function() {
     var $text = $("#screen").text();
-    if($(this).text() === "x"){
+
+    if ($(this).text() === "x") {
       $text += "*";
     }
-    else if($(this).text() === "\367"){
+    else if ($(this).text() === "\367") {
       $text += "/";
     }
     else {
       $text += $(this).text();
     }
     $("#screen").text($text);
-  })
+  });
 
-  $("#cancel").click(function(){
+  $("#cancel").click(function() {
     $("#screen").text("");
-  })
+  });
 
-  $("#calc").click(function(){
+  $("#calc").click(function() {
     var $text = $("#screen").text().slice(0, -1);
+
     $text = eval($text);
     $("#screen").text($text);
-  })
+  });
 });
