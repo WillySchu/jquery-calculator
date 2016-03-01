b2e1d0462a2f87a46dea8ec0e32ef9d8e569a2e4
Will Schumacher <meintodengel@gmail.com>
2016-03-01 08:20:38 -0800

    Add solution

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+  $("span").click(function(){
+    var $text = $("#screen").text();
+    if($(this).text() === "x"){
+      $text += "*";
+    }
+    else if($(this).text() === "\367"){
+      $text += "/";
+    }
+    else {
+      $text += $(this).text();
+    }
+    $("#screen").text($text);
+  })
+
+  $("#cancel").click(function(){
+    $("#screen").text("");
+  })
+
+  $("#calc").click(function(){
+    var $text = $("#screen").text().slice(0, -1);
+    $text = eval($text);
+    $("#screen").text($text);
+  })
+});
