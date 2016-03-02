502bb37c7af9b6cf240f0439a1774eaef42b1b40
Will Schumacher <meintodengel@gmail.com>
2016-03-02 14:23:26 -0800

    Change variable name

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
 
   $("span").click(function() {
-    var $text = $("#screen").text();
-    console.log(!$.isNumeric($text[$text.length - 1]));
-    if($(this).hasClass("operator") && !$.isNumeric($text[$text.length - 1])){
-      $text = $text.slice(0, -1);
+    var text = $("#screen").text();
+    console.log(!$.isNumeric(text[text.length - 1]));
+    if($(this).hasClass("operator") && !$.isNumeric(text[text.length - 1])){
+      text = text.slice(0, -1);
     }
 
     if ($(this).text() === "x") {
-      $text += "*";
+      text += "*";
     }
     else if ($(this).text() === "\367") {
-      $text += "/";
+      text += "/";
     }
     else {
-      $text += $(this).text();
+      text += $(this).text();
     }
-    $("#screen").text($text);
+    $("#screen").text(text);
   });
 
   $("#cancel").click(function() {
@@ -24,9 +24,9 @@ $(document).ready(function() {
   });
 
   $("#calc").click(function() {
-    var $text = $("#screen").text().slice(0, -1);
+    var text = $("#screen").text().slice(0, -1);
 
-    $text = eval($text);
-    $("#screen").text($text);
+    text = eval(text);
+    $("#screen").text(text);
   });
 });
